5a072a33ddabc57bd329e42d573dd97ec7800922
Pany Koutsios <pkoutsios@hotmail.com>
2014-06-29 10:29:39 +0100

    animations

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+var animEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
 $(document).ready(function() {
 	onResize();	//initialises
 	$(window).resize(onResize); //on browser size change
 	
 	
-	
+	// rollover projects
 	$(".project").mouseover(function () {
 		$(this).children(".p-inner").fadeIn();
 	})
@@ -11,15 +13,36 @@ $(document).ready(function() {
 		$(this).children(".p-inner").fadeOut(200);
 	})
 	.mousedown(function() {
-		$(this).parent().fadeOut(600);
-		$("#project-details").fadeIn(600);
+		var projectDetails = $("#project-details");
+		var details = $(".template .project-details-container").clone();
+		projectDetails.empty();
+		//
+		//fill in details
+		//
+		projectDetails.append(details);
+		
+		
+		
+		
+		
+		// slideOut projects and slideIn project details
+		$("#projects").removeClass("animated slideInLeft").addClass("animated slideOutLeft").one(animEnd, function () {
+			$(this).hide().removeClass("animated slideOutLeft");
+			projectDetails.removeClass("animated slideOutRight").show().addClass("animated slideInRight").one(animEnd, function() {
+				$(this).removeClass("animated slideInRight");
+			});
+		});
 	});
 	
 	
-	
-	$(".back").mousedown(function() {
-		$("#project-details").fadeOut(600);
-		$("#projects").fadeIn(600);
+	// slideOut project details and slideIn projects
+	$("#project-details").on("click", ".back", function() {
+		$("#project-details").removeClass("animated slideInRight").addClass("animated slideOutRight").one(animEnd, function() {
+			$(this).hide().removeClass("animated slideOutRight");
+			$("#projects").show().addClass("animated slideInLeft").one(animEnd, function() {
+				$(this).removeClass("animated slideInLeft");
+			});
+		});
 	});
 });
 
@@ -33,3 +56,12 @@ var onResize = function () {
 		//$("nav div").css("width", winHieght * 0.05);
 	}
 }
+
+var projDetailCollection = [{
+							"World Cup API": [{
+								"skills": "HTML, CSS, JavaScript, jQuery, AJAX, JSON",
+								"github": "https://github.com/Peekay777/worldcupapihack",
+								"link": "http://peekay777.github.io/worldcupapihack/",
+								"screeny": "worldcupscreeny.jpg"
+							}]
+}];
